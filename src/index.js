bae721dc41dfcfe66e47d77bd0c26b5c64a0edb4
kobayskiy <sopsip51@gmail.com>
2025-10-24 13:02:18 +0300

    second step

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const genDiff = (filepath1, filepath2) => {
-  return `Comparing ${filepath1} and ${filepath2}`;
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
 };
 
 export default genDiff;
